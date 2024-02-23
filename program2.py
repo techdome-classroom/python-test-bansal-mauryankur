403d1c9f4bdcdc741025a0bdb300f7f15b3d3d05
ANKUR MAURYA <134962640+mauryankur@users.noreply.github.com>
2024-02-23 13:23:48 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,25 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
         :type s: str
         :rtype: int
-        """
-        pass
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+        
+        for symbol in s:
+            value = roman_values[symbol]
+            if value > prev_value:
+                total += value - 2 * prev_value 
+            else:
+                total += value
+            prev_value = value
+        
+        return total
+
+
+
+solution = Solution()
+print(solution.romanToInt("III"))     
+print(solution.romanToInt("LVIII"))    
+print(solution.romanToInt("MCMXCIV")) 
+
