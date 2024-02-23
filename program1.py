16e77db57801ecca43427cc0bc611d2c1760c419
ANKUR MAURYA <134962640+mauryankur@users.noreply.github.com>
2024-02-23 13:34:37 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,20 +1,24 @@
 class Solution(object):
     def isValid(self, s):
         stack=[]
-        mapping = { ')' : (' , '}' : '{' , ']' : '['}
+        mapping = {')': '(', '}': '{', ']': '['}
         
         for char in s:
             if char in mapping:
-                 top_element = stack.pop() if stack else '#'
-                 if  mapping[char] != top_element:
-                     return False
-             else :
-                 stack.append(char)
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
 
-          return not stack
- print(isValid("()"))
- print(isValid("()[]{}"))
- print(isValid("{)"))
+
+solution = Solution()
+print(solution.isValid("()"))       
+print(solution.isValid("()[]{}"))    
+print(solution.isValid("{)"))        
+
     
 
 
